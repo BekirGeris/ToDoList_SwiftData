c754ca8fe254ea3dab8d39c9bf93c07c96866abd
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2023-10-04 12:53:31 +0300

    Initial Commit

---
+++ ToDoListSwiftData/ToDoListSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftDataApp.swift
+//  ToDoListSwiftData
+//
+//  Created by Bekir Geriş on 4.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
